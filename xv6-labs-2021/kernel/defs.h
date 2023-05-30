749c9798496cae4a9fe15f5a88d8dc14eaa3c2d4
Cocoson23 <Cocoson23@outlook.com>
2023-05-30 11:12:36 +0800

    lab3 print page table

--- xv6-labs-2021/kernel/defs.h
+++ xv6-labs-2021/kernel/defs.h
@@ -171,6 +171,8 @@ int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
 
+void            vmprint(pagetable_t);
+
 // plic.c
 void            plicinit(void);
 void            plicinithart(void);
--- xv6-labs-2021/kernel/exec.c
+++ xv6-labs-2021/kernel/exec.c
@@ -116,6 +116,10 @@ exec(char *path, char **argv)
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
 
+  // lab3 print page table
+  if(p->pid == 1)
+      vmprint(p->pagetable);
+  
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
@@ -125,6 +129,7 @@ exec(char *path, char **argv)
     iunlockput(ip);
     end_op();
   }
+
   return -1;
 }
 
