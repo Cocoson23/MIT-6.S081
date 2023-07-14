6ac195dae6f52559f564b39c2476f1dc5208c17a
Cocoson23 <Cocoson23@outlook.com>
2023-07-14 22:22:05 +0800

    lab lock finish

--- xv6-labs-2021/kernel/kalloc.c
+++ xv6-labs-2021/kernel/kalloc.c
@@ -18,15 +18,21 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct kmem{
   struct spinlock lock;
   struct run *freelist;
 } kmem;
+// 将原list拆分为N个CPU单独使用的多个小list
+// 且每个小list单独使用一个lock
+struct kmem kmems[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  // 初始化所有CPU的小list lock
+  for(int i = 0; i < NCPU; i++)
+      initlock(&(kmems[i].lock), "kmem");
+  
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,6 +41,7 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
+
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
 }
@@ -56,10 +63,19 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  // 将pa页插入到当前CPU的list中 
+  // get cpu id
+  int cpu_id;
+  // 关中断
+  push_off();
+  cpu_id = cpuid();
+  acquire(&(kmems[cpu_id]).lock);
+  r->next = kmems[cpu_id].freelist;
+  kmems[cpu_id].freelist = r;
+  release(&(kmems[cpu_id]).lock);
+
+  // 开中断
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,12 +86,37 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  // 关中断，避免同时获取两个lock发生死锁
+  push_off();
+  int cpu_id = cpuid();
 
+  acquire(&(kmems[cpu_id]).lock);
+  r = kmems[cpu_id].freelist;
+  
+  // 若当前CPU list中有free的page则直接分配
+  if(r) {
+    kmems[cpu_id].freelist = r->next;
+  }
+  // 若无则去其余CPU的list中寻找并插入当前list
+  else {
+      for(int i = 0 ; i < NCPU; i++) {
+          if(i == cpu_id)
+              continue;
+          // 遍历获取其余CPU id
+          acquire(&(kmems[i]).lock);
+          r = kmems[i].freelist;
+          if(r) {
+              kmems[i].freelist = r->next;
+              release(&(kmems[i]).lock);
+              break;
+          }
+          release(&(kmems[i]).lock);
+      }
+  }
+
+  release(&(kmems[cpu_id]).lock);
+  // 开中断
+  pop_off();
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
