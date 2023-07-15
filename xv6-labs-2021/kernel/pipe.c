aceee3ea0c5b9fbcb73eaf9a60e21768aa242767
Cocoson23 <Cocoson23@outlook.com>
2023-07-15 20:29:38 +0800

    switch to lab filesystem

--- xv6-labs-2021/kernel/pipe.c
+++ xv6-labs-2021/kernel/pipe.c
@@ -68,9 +68,6 @@ pipeclose(struct pipe *pi, int writable)
   }
   if(pi->readopen == 0 && pi->writeopen == 0){
     release(&pi->lock);
-#ifdef LAB_LOCK
-    freelock(&pi->lock);
-#endif    
     kfree((char*)pi);
   } else
     release(&pi->lock);
